d9301354555d4a8675d03121afad4a1130cce494
Miloslav Jezek <milo.jezek@gmail.com>
2025-09-29 13:52:04 +0200

    Update text content and make favicon clearer for both dark and light mode

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,16 @@
     </nav>
   </header>
   <main>
-    <h1>Web Development, JavaScript, TypeScript, ReactJS, VueJS, NodeJS</h1>
+    <h1>Full-stack Development, JavaScript, TypeScript, NodeJS, ReactJS, VueJS</h1>
     <ProjectsSection />
     <section id="about">
       <p
         class="about-text"
-      >I’m Miloslav, a full stack developer based in Prague (Czechia) with over 3 years of experience in web development. I specialize in building web apps and websites. My go-to languages are JavaScript and TypeScript, but I'm always open to learning and using whatever is best for the job at hand. Lately, I've been diving into Go to expand my toolkit.</p>
+      >I’m Miloslav Jezek, a full stack developer based in Prague (Czechia) with over 3 years of experience in building web applications and websites. My go-to programming languages are JavaScript and TypeScript, but I'm always open to learning and using whatever is best for the job at hand.</p>
     </section>
+    <p class="about-text links">
+      <a>LinkedIn</a>
+    </p>
   </main>
   <footer>
     <p class="footer-text">© Miloslav Jezek 2025</p>
