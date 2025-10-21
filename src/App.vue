c90221b075643ac445163728bb99ebf850958ad7
Miloslav Jezek <miloslav.jezek@fpt.sk>
2025-10-21 11:49:41 +0200

    Remove inline styling in App.vue and make define the remove styles in styles.css

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
     </header>
     <main>
       <h1 class="fade-in fade-in-delay-1">Full-stack Developer</h1>
-      <h1 class="fade-in fade-in-delay-2" style="color: #5a5a5a">JavaScript & TypeScript</h1>
-      <h1 class="fade-in fade-in-delay-3" style="color: #5a5a5a">Node | Express | React | Vue</h1>
+      <h1 class="fade-in fade-in-delay-2 subtitle">JavaScript & TypeScript</h1>
+      <h1 class="fade-in fade-in-delay-3 subtitle">Node | Express | React | Vue</h1>
       <div class="fade-in fade-in-delay-4">
         <ProjectsSection />
       </div>
@@ -37,8 +37,7 @@
             class="about-text"
           >I'm Miloslav Jezek, a developer based in Prague with experience building full-stack TypeScript web applications. JS & TS are my go-to languages, but I'm always happy to use whatever is most suitable for the job at hand. The backend is the area where I feel best and where I direct my efforts and growth, but I also enjoy building clean, easy-to-navigate user interfaces.</p>
           <p
-            class="about-text"
-            style="margin-top: 1.5rem;"
+            class="about-text about-text-spacing"
           >This portfolio is my small digital space where I'll showcase my recent personal work. I'm particularly interested in projects related to languages, travelling, health, and anything that makes living easier. Whether it's a complete app or a small utility tool.</p>
         </div>
       </section>
